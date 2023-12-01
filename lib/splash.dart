cf295c31cd7e20f1518bbaba3e3552c411ebc31d
lumix00 <lumix00gamer@gmail.com>
2023-11-30 21:53:44 -0300

    Fix do erro da animacao
    Movi a animacao para a tela de login ao inves da de splash, assim consegui manter o background fixo e so movimentar o formulario

--- lib/splash.dart
+++ lib/splash.dart
@@ -8,55 +8,22 @@ class Splash extends StatefulWidget {
   State<Splash> createState() => _SplashState();
 }
 
-class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<Offset> _offsetAnimation;
-
+class _SplashState extends State<Splash> {
   @override
   void initState() {
     super.initState();
-
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 500),
-    );
-
-    _offsetAnimation = Tween<Offset>(
-      begin: const Offset(0.0, 1.0),
-      end: const Offset(0.0, 0.0),
-    ).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.easeInOut,
-      ),
-    );
-
-    _navigateToHome();
+    _navigateToLogin();
   }
 
-  _navigateToHome() async {
+  _navigateToLogin() async {
     await Future.delayed(const Duration(seconds: 1));
-    _controller.forward();
 
-    await Future.delayed(
-        const Duration(milliseconds: 500)); // Tempo para a animação
-
-    // ignore: use_build_context_synchronously
     Navigator.pushReplacement(
       context,
-      PageRouteBuilder(
-        pageBuilder: (context, animation1, animation2) => const Login(
+      MaterialPageRoute(
+        builder: (context) => Login(
           backgroundImage: 'assets/back.png',
         ),
-        transitionsBuilder: (context, animation1, animation2, child) {
-          return SlideTransition(
-            position: Tween<Offset>(
-              begin: const Offset(0.0, 1.0),
-              end: const Offset(0.0, 0.0),
-            ).animate(animation1),
-            child: child,
-          );
-        },
       ),
     );
   }
@@ -71,19 +38,12 @@ class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
             fit: BoxFit.cover,
           ),
         ),
-        child: SlideTransition(
-          position: _offsetAnimation,
-          child: Container(
-            color: Colors.transparent,
-          ),
-        ),
       ),
     );
   }
 
   @override
   void dispose() {
-    _controller.dispose();
     super.dispose();
   }
 }
